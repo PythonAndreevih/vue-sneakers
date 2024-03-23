8aaa7855f50db4325e900173fceff70352181c57
PythonAndreevih <kodokuvatashi@gmail.com>
2024-03-24 00:20:43 +0300

    Добавил marg

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,7 +18,7 @@ const emit = defineEmits(['openDrawer'])
       </div>
     </RouterLink>
 
-    <ul class="flex items-center  md: flex-wrap gap-0 lg:gap-10">
+    <ul class="flex items-center md: flex-wrap ml-10 gap-0 lg:gap-10">
       <li
         @click="() => emit('openDrawer')"
         class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black"
